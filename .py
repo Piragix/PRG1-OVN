bd41c6040b8be1228944228b61162219579238f7
Piragix <90191529+Piragix@users.noreply.github.com>
2021-09-17 13:23:42 +0200

    yes

---
+++ .py
@@ -0,0 +1,14 @@
+a = 12
+b = 12.0
+c = '12.0'
+
+print(f"Jag kan räkna till {a} ")
+print(f"Jag kan räkna till {b} ")
+print(f"Jag kan räkna till {c} ")
+
+aType = type(a)
+
+# if(aType == "<class 'int'>")
+
+print(f"är en {aType}")
+

65f1f85ee30f8e23137428f7e876b0b36761e346
Piragix <90191529+Piragix@users.noreply.github.com>
2021-09-23 09:41:22 +0200

    WIP

--- bränsleekonomi.py
+++ bränsleekonomi.py
@@ -1,3 +1,16 @@
-mätarstl_nu = input("Mätarställning i dag? ")
-mätarstl_förr = input("Mätarställning för ett år sen? ")
-bensin = input("Antal liter bensin använt? ")
+svar1 = input("Mätarställning i dag? ")
+svar2 = input("Mätarställning för ett år sen? ")
+svar3 = input("Antal liter bensin använt? ")
+
+materstl_nu = float(svar1)
+matarstl_forr = float(svar2)
+bensin = float(svar3)
+
+mil = matarstl_nu - matarstl_forr
+
+litermilen = bensin / mil
+
+
+print(f"Antal körda mil: {mil}")
+print(f"Antal liter bensin: {bensin}")
+print(f"Förbrukning per mil: {litermilen}")

4d2108c3ee975f788e23bebe3202d882b15f9f90
Piragix <90191529+Piragix@users.noreply.github.com>
2021-10-05 08:28:07 +0200

    yes

---
+++ Dansamedtonyellernått.py
@@ -0,0 +1,29 @@
+print("Snart slutar vi\nVad kommer du göra när du kommer hem?")
+print(" [1] Sova\n [2] Spela\n [3] Dränka din sorg med cola\n [4] ( ͡° ͜ʖ ͡°)\n [5] Titta på anime\n [6] Ingenting " )
+svar = ""
+
+while svar != "försvinn":
+    svar = input("Vad ska du göra? ")
+
+    if svar == "1":
+        print("Heter du Noah?")
+
+    if svar == "2":
+        print("Bra val")
+
+    if svar == "3":
+        print("Agel moment")
+
+    if svar == "4":
+        print("Ah yes, a man of culture")
+
+    if svar == "5":
+        print("Jävla weeb")
+
+    if svar == "6":
+        print("Jahadå")
+    
+    
+    else: 
+        print("Ska du göra något mer?")
+        
---
+++ åldergrej.py
@@ -0,0 +1,14 @@
+
+age = 100
+
+while age > 18:
+    age = int(input("Hur gammal är du?: "))
+
+    if age < 18:
+        print("Du är under 18 år gammal")
+
+    if age == str:
+        print("Du måste skriva ett tal")
+
+    else:
+        print("Du är över 18 år gammal")
